1cf01ed35b7c9ae7f0512071d0a1ef89a66b4e27
Rizki, Alfian <alfian.rizki@ecomindo.com>
2021-06-19 19:53:34 +0700

    db connection update

--- util/db.go
+++ util/db.go
@@ -11,23 +11,8 @@ import (
 
 //NewDBClient to create new connection
 func NewDBClient() (*sql.DB, error) {
-	var (
-		dbUser                 = os.Getenv("DB_USER")                  // e.g. 'my-db-user'
-		dbPwd                  = os.Getenv("DB_PASS")                  // e.g. 'my-db-password'
-		instanceConnectionName = os.Getenv("INSTANCE_CONNECTION_NAME") // e.g. 'project:region:instance'
-		dbName                 = os.Getenv("DB_NAME")                  // e.g. 'my-database'
-	)
-
-	socketDir, isSet := os.LookupEnv("DB_SOCKET_DIR")
-	if !isSet {
-		socketDir = "/cloudsql"
-	}
-
 	var dbURI string
-	dbURI = fmt.Sprintf("%s:%s@unix(%s/%s)/%s?parseTime=true", dbUser, dbPwd, socketDir, instanceConnectionName, dbName)
-	if os.Getenv("APP_MODE") == "DEBUG" {
-		dbURI = os.Getenv("CONNECTION_STRING")
-	}
+	dbURI = os.Getenv("CONNECTION_STRING")
 
 	// dbPool is the pool of database connections.
 	db, err := sql.Open("mysql", dbURI)
