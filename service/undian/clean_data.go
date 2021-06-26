f30cbe9d40d36501736d8e0aad25b908b3bd2510
Rizki, Alfian <alfian.rizki@ecomindo.com>
2021-06-26 12:07:37 +0700

    change from query row to query

--- service/undian/clean_data.go
+++ service/undian/clean_data.go
@@ -6,7 +6,8 @@ import (
 
 func (s *service) CleanPemenang(ctx context.Context) error {
 	sql := `UPDATE pemenang SET deleted = 1`
-	err := s.db.QueryRowContext(ctx, sql).Err()
+
+	_, err := s.db.QueryContext(ctx, sql)
 	if err != nil {
 		return err
 	}
